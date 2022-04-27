218857635c2803a4241f72bade284d50e07732a8
Shreya Banerjee <67795151+Shreya1805-web@users.noreply.github.com>
2022-04-28 00:13:51 +0530

    Added files

---
+++ Ecommerce-app-h/footer.php
@@ -0,0 +1,3 @@
+<footer style="background:#302ea3; color:white; padding:10px 20px;">
+  <p>&copy; ShopEzy, Ltd. All Rights Reserved.</p>
+</footer>
---
+++ Ecommerce-app-h/templates/footer.php
@@ -0,0 +1,11 @@
+<link rel = "stylesheet" href="https://cdnjs.cloudfare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+<style type  = text/css">
+    .brand{
+        background: #cbb09c;
+    }
+    .brand-text{
+        color: #cbb09c;
+    }
+<footer class= "section">
+     <div class = "center grey-text">&copy; Copyright 2022 Shopezy </div>
+</footer>
